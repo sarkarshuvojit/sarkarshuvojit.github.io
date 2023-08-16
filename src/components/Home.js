d4f7fd8283d2cf44963426436df64d88f37c13fc
Shuvojit <s15sarkar@yahoo.com>
2023-08-16 22:13:43 +0530

    wip

---
+++ src/components/Home.js
@@ -0,0 +1,24 @@
+const Home = () => {
+  return (
+    <section id="home">
+      <div className="container">
+        <div className="roww">
+          {/* Main Title */}
+          <div className="resumo_fn_main_title">
+            <h3 className="subtitle">Introduction</h3>
+            <h3 className="title">UI/UX Designer</h3>
+            <p className="desc">
+              I design and develop services for customers of all sizes,
+              specializing in creating stylish, modern websites, web services
+              and online stores.
+            </p>
+            <img src="img/signature.png" alt="image" />
+          </div>
+          {/* /Main Title */}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
