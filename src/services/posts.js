c686dba57b20d475f4ba34dbf4118aae425f809d
Shuvojit <s15sarkar@yahoo.com>
2023-09-22 20:06:45 +0530

    added date formatting function

--- src/services/posts.js
+++ src/services/posts.js
@@ -1,9 +1,22 @@
 import {useQuery} from "react-query";
 
+function getNicerFormat(ds) {
+  const d = new Date(ds);
+  const m = d.toLocaleString('default', { month: 'long' });
+  return `${m} ${d.getDate()}, ${d.getFullYear()}`;
+}
+
 async function getAllPosts() {
   const response = await fetch("https://7aha5s5ytznht4zwgd7zmhjwpa0zxydq.lambda-url.us-east-1.on.aws/");
   const text = await response.text();
-  return JSON.parse(text)["posts"];
+  const _posts = JSON.parse(text)["posts"];
+  _posts.forEach(_p => {
+    console.log(_p)
+    console.log(_p.PublishedAt);
+    _p.PublishedAt = getNicerFormat(_p.PublishedAt)
+    console.log(_p)
+  })
+  return _posts;
 }
 
 export function usePosts() {
