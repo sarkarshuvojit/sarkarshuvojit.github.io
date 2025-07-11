10247f209915cf07ffb1f0ef78f9cc66c270e92f
Shuvojit Sarkar <s15sarkar@yahoo.com>
2025-07-11 23:40:29 +0530

    added tooltips to social icons

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -5,37 +5,37 @@ const Socials = () => {
   return <div className="social">
     <ul className="flex justify-center items-center">
       <li>
-        <a href={texts.urls.github} target="_blank">
+        <a href={texts.urls.github} target="_blank" title="GitHub">
           <Github />
         </a>
       </li>
 
       <li>
-        <a href={texts.urls.stackoverflow} target="_blank">
+        <a href={texts.urls.stackoverflow} target="_blank" title="Stack Overflow">
           <Stackoverflow />
         </a>
       </li>
 
       <li>
-        <a href={texts.urls.twitter} target="_blank">
+        <a href={texts.urls.twitter} target="_blank" title="Twitter">
           <Twitter />
         </a>
       </li>
 
       <li>
-        <a href={texts.urls.linkedin} target="_blank">
+        <a href={texts.urls.linkedin} target="_blank" title="LinkedIn">
           <Linkedin />
         </a>
       </li>
 
       <li>
-        <a href={texts.urls.email} target="_blank">
+        <a href={texts.urls.email} target="_blank" title="Email">
           <Email />
         </a>
       </li>
 
       <li>
-        <a href={texts.urls.resume} target="_blank" download>
+        <a href={texts.urls.resume} target="_blank" download title="Download Resume">
           <Download />
         </a>
       </li>
