a5633ac639d70a877516d81d0db73722c9e91c9f
Shuvojit Sarkar <s15sarkar@yahoo.com>
2025-07-11 23:04:03 +0530

    updated intro

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
             <h3 className="subtitle">Introduction</h3>
             <h3 className="title">Welcome Aboard</h3>
             <p className="desc">
-              Greetings and a warm welcome to my digital abode, I’m Shuvojit Sarkar, and I'm delighted you've chosen to visit. I serve the technology sector as a seasoned full-stack developer, specializing in backend and devops. My daily work involves building & running scalable and distributed backend services.
+              Hi, I'm Shuvojit Sarkar. I love working across the stack but focus mainly on backend development, building scalable systems and distributed services. When I'm not coding, you'll find me exploring keyboards, travelling, or enjoying nature.
             </p>
           </div>
           {/* /Main Title */}
